988637dddd2ef5b8719988d8cb1e54539ece5082
Sebastian Langer <seb.langer@outlook.com>
2019-10-22 12:32:12 +0200

    added boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # read
+  # read all
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+ # create
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
